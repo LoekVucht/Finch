a67b9137cc91089faf39f864eef5ec8894a4e3b8
LoekVucht <lj.vanvucht@student.han.nl>
2022-10-16 23:07:51 +0200

    dingen doen op basis van invoer (test)

--- Finch/src/nl/han/aim/oose/ooad/finch/FinchSpel.java
+++ Finch/src/nl/han/aim/oose/ooad/finch/FinchSpel.java
@@ -2,21 +2,42 @@ package nl.han.aim.oose.ooad.finch;
 
 import java.util.Scanner;
 
-public class Finch {
+public class FinchSpel {
+    private Gebruiker[] gebruikers;
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // Test voor hoe we de invoer kunnen regelen
-        System.out.println("Hello world!");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welkom bij Finch! Typ \"R\" om je te registreren of \"I\" om in te loggen met een bestaand account.");
+        while (true) {
+            String keuze = krijgInvoer();
+            if (keuze.equals("R")) {
+                registreer(scanner);
+                break;
+            } else if (keuze.equals("I")) {
+                System.out.println("inloggen");
+                // todo
+                break;
+            } else {
+                System.out.println("Typ \"R\" of \"I\".");
+            }
+        }
+        scanner.close();
+    }
+
+    private static String krijgInvoer() {
+        return scanner.nextLine();
+    }
+
+    private static void registreer(Scanner scanner) {
         System.out.println("Gebruikersnaam is " + krijgGebruikersnaamVanInvoer(scanner));
         System.out.println("Wachtwoord is " + krijgWachtwoordVanInvoer(scanner));
-        scanner.close();
     }
 
     private static String krijgWachtwoordVanInvoer(Scanner scanner) {
         String wachtwoord;
         while (true) {
             System.out.println("Voer een wachtwoord in.");
-            wachtwoord = scanner.nextLine();
+            wachtwoord = krijgInvoer();
             if (!wachtwoord.isBlank()) {
                 break;
             }
@@ -28,7 +49,7 @@ public class Finch {
         String gebruikersnaam;
         while (true) {
             System.out.println("Voer een gebruikersnaam in.");
-            gebruikersnaam = scanner.nextLine();
+            gebruikersnaam = krijgInvoer();
             if (gebruikersnaam.equals("bestaat al")) {
                 System.out.println("Deze gebruikersnaam bestaat al.");
                 continue;
---
+++ Finch/src/nl/han/aim/oose/ooad/finch/Gebruiker.java
@@ -0,0 +1,11 @@
+package nl.han.aim.oose.ooad.finch;
+
+public class Gebruiker {
+    private String gebruikersnaam;
+    private String wachtwoord;
+
+    public Gebruiker(String gebruikersnaam, String wachtwoord) {
+        this.gebruikersnaam = gebruikersnaam;
+        this.wachtwoord = wachtwoord;
+    }
+}
