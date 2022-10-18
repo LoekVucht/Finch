8744aaca70c9106ae4cbeb9ead5923e6c8c69842
joran maaskant <joranmaaskant@hotmail.com>
2022-10-18 14:55:33 +0200

    test omgedraaid

--- Finch/src/nl/han/aim/oose/ooad/finch/FinchSpel.java
+++ Finch/src/nl/han/aim/oose/ooad/finch/FinchSpel.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class FinchSpel {
     private Gebruiker[] gebruikers;
-    private int iscool;
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
