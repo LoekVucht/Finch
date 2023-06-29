f8cc607a9e89c221b03ca154a8ae673de321e0a6
LoekVucht <94004136+LoekVucht@users.noreply.github.com>
2023-06-29 23:03:31 +0200

    BetalingsserviceAdapter toegevoegd

---
+++ src/nl/han/aim/oose/ooad/finch/BetalingsserviceAdapter.java
@@ -0,0 +1,5 @@
+package nl.han.aim.oose.ooad.finch;
+
+public interface BetalingsserviceAdapter {
+    void betaal();
+}
---
+++ src/nl/han/aim/oose/ooad/finch/NederlandsBankenSysteem.java
@@ -0,0 +1,8 @@
+package nl.han.aim.oose.ooad.finch;
+
+public class NederlandsBankenSysteem implements BetalingsserviceAdapter {
+    @Override
+    public void betaal() {
+
+    }
+}
